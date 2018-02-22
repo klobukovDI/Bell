e7b89ec7bc20b729452603e92b1ee70c840856fd
klobukovDI <34067401+klobukovDI@users.noreply.github.com>
2018-02-22 08:30:37 +0200

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS House (
     id         INTEGER  PRIMARY KEY AUTO_INCREMENT,
     version    INTEGER NOT NULL,
     address    VARCHAR(50) NOT NULL
+    
 );
 
 CREATE INDEX IX_Person_House_Id ON Person (house_id);
